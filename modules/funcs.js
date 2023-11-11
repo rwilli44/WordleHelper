d2dd563cef2cbea7057c1c58b9574ad24eeee644
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-11 11:12:23 +0100

    changed correct pos from dict to list to allow letters to repeat

--- modules/funcs.js
+++ modules/funcs.js
@@ -1,13 +1,12 @@
 "set strict";
 import { word_list } from "./const.js";
 
-export function find_word(list_elim_letters, correct_dict, incorrect_dict) {
-  let possible_words = ["test"];
-  console.log(possible_words);
+export function find_word(list_elim_letters, correct_list, incorrect_dict) {
+  let possible_words = [];
   for (var i in word_list) {
     var word = word_list[i].toUpperCase();
     var has_eliminated_letters = check_eliminated(word, list_elim_letters);
-    var correct_letters_in_place = check_correct_pos(word, correct_dict);
+    var correct_letters_in_place = check_correct_pos(word, correct_list);
     var letters_in_incorrect_place = check_incorrect_pos(word, incorrect_dict);
 
     if (
@@ -24,21 +23,25 @@ export function find_word(list_elim_letters, correct_dict, incorrect_dict) {
 
 function check_eliminated(word, list_elim_letters) {
   for (var letter in list_elim_letters) {
-    if (word.includes(list_elim_letters[letter].trim())) {
+    if (word.includes(list_elim_letters[letter])) {
       return true;
     }
   }
   return false;
 }
 
-function check_correct_pos(word, correct_dict) {
-  for (let key in correct_dict) {
-    let letter_pos = parseInt(correct_dict[key]) - 1;
+function check_correct_pos(word, correct_list) {
+  for (let i in correct_list) {
+    let correct_letter = correct_list[i][0];
+    let letter_pos = parseInt(correct_list[i][1]) - 1;
     let word_letter = word[letter_pos];
-    if (key.trim() != word_letter) {
+    console.log(correct_letter);
+    console.log(word_letter);
+    if (correct_letter != word_letter) {
       return false;
     }
   }
+  console.log("match");
   return true;
 }
 
@@ -46,11 +49,13 @@ function check_incorrect_pos(word, incorrect_dict) {
   for (let key in incorrect_dict) {
     let letter_pos = parseInt(incorrect_dict[key]) - 1;
     let word_letter = word[letter_pos];
-    if (key.trim() == word_letter) {
+    if (key == word_letter) {
       return true;
-    } else if (!word.includes(key.trim())) {
+    } else if (!word.includes(key)) {
       return true;
     }
   }
   return false;
 }
+
+// clean up white space issues, make it tuples or lists instead of dict to have letter used more than once, clean up functions
--- wordlehelper.js
+++ wordlehelper.js
@@ -13,35 +13,41 @@ import { find_word } from "./modules/funcs.js";
 
 var list_elim_letters, list_correct_pos, list_incorrect_pos;
 var incorrect_dict = {};
-var correct_dict = {};
+var correct_list = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 find_button.addEventListener("click", () => {
-  let pattern_eliminated = /^[a-zA-Z](?:,\s*[a-zA-Z])*$/;
-  let pattern_positions = /^[a-zA-Z]:[1-5](,\s*[a-zA-Z]:[1-5])*$/;
-  if (pattern_eliminated.test(eliminated_letters.value)) {
-    list_elim_letters = eliminated_letters.value.split(",");
+  let pattern_elim_letters = /^[a-zA-Z](?:,\s*[a-zA-Z])*$/;
+  let pattern_letter_pos = /^[a-zA-Z]:[1-5](,\s*[a-zA-Z]:[1-5])*$/;
+  if (pattern_elim_letters.test(eliminated_letters.value)) {
+    list_elim_letters = eliminated_letters.value;
+    list_elim_letters = list_elim_letters.replaceAll(" ", "");
+    list_elim_letters = list_elim_letters.split(",");
   }
-  if (pattern_positions.test(correct_position.value)) {
+  if (pattern_letter_pos.test(correct_position.value)) {
     let pos_pairs = correct_position.value.split(",");
     for (let i in pos_pairs) {
-      var pair_info = pos_pairs[i].split(":");
-      correct_dict[pair_info[0]] = pair_info[1];
+      var pair_info = pos_pairs[i];
+      pair_info = pair_info.replaceAll(" ", "");
+      pair_info = pair_info.split(":");
+      correct_list.push(pair_info);
     }
   }
-  if (pattern_positions.test(incorrect_position.value)) {
+  if (pattern_letter_pos.test(incorrect_position.value)) {
     let pos_pairs = incorrect_position.value.split(",");
     for (let i in pos_pairs) {
-      var pair_info = pos_pairs[i].split(":");
+      var pair_info = pos_pairs[i];
+      pair_info = pair_info.replaceAll(" ", "");
+      pair_info = pair_info.split(":");
       incorrect_dict[pair_info[0]] = pair_info[1];
     }
   }
   let possible_words = find_word(
     list_elim_letters,
-    correct_dict,
+    correct_list,
     incorrect_dict
   );
   console.log(possible_words);
