b0f8d94c55ab0fd562d62ff75f05f21c5dcd2ee7
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-13 00:26:44 +0100

    refactoring and commenting, dealt with duplicate letters that shouldn't be in elim list

--- modules/funcs.js
+++ modules/funcs.js
@@ -1,7 +1,107 @@
 "set strict";
 import { word_list } from "./wordlist.js";
-// import { word_list } from "./modules/list.js";
+import {
+  num_of_guesses,
+  all_guess_inputs,
+  letter_squares,
+  all_guess_rows,
+  number_found_p,
+  words_found_p,
+  reveal_button,
+  rows_letter_tiles,
+} from "./const.js";
 
+/*************************** Responding to Input ***************************/
+
+export function show_rows(number_guesses) {
+  for (let j = 0; j < number_guesses; j++) {
+    all_guess_rows[j].style.display = "flex";
+  }
+}
+export function change_color(clickedElement) {
+  if (clickedElement.localName == "p" && clickedElement.innerText != "") {
+    let current_bgColor = clickedElement.style.backgroundColor;
+    if (current_bgColor == "yellow") {
+      clickedElement.style.backgroundColor = "lime";
+    } else if (current_bgColor == "lime") {
+      clickedElement.style.backgroundColor = "lightgrey";
+    } else if (current_bgColor == "" || current_bgColor == "lightgrey") {
+      clickedElement.style.backgroundColor = "yellow";
+    }
+  }
+}
+
+export function show_letters(row, inputElement) {
+  let alpha_regex = /^[a-zA-Z]+$/;
+  let letter_position = 0;
+  for (const child of row.childNodes[3].children) {
+    if (
+      child.localName == "p" &&
+      letter_position < inputElement.value.length &&
+      alpha_regex.test(inputElement.value)
+    ) {
+      child.textContent = inputElement.value[letter_position].toUpperCase();
+      child.style.backgroundColor = "lightgrey";
+      letter_position++;
+    } else if (child.localName == "p") {
+      child.textContent = "";
+      child.style.backgroundColor = "lightgrey";
+      letter_position++;
+    }
+  }
+}
+
+/*************************** Clearing Input ***************************/
+
+// Reset all user input
+export function reset_input() {
+  num_of_guesses.value = 1;
+  for (let i = 0; i < 5; i++) {
+    all_guess_inputs[i].value = "";
+  }
+  for (let i = 0; i < 25; i++) {
+    letter_squares[i].innerText = "";
+  }
+
+  for (let i = 1; i <= 4; i++) {
+    all_guess_rows[i].style.display = "None";
+  }
+}
+
+// Reset all tiles to grey/blank
+export function reset_all_tiles() {
+  for (let i = 0; i < 25; i++) {
+    letter_squares[i].style.backgroundColor = "lightgrey";
+  }
+}
+
+// Hide unneeded rows
+export function reset_rows(number_guesses) {
+  for (let j = number_guesses; j < 5; j++) {
+    let row = all_guess_rows[j];
+    row.style.display = "none";
+    all_guess_inputs[j].value = "";
+    reset_row_tiles(j);
+  }
+}
+
+// Reset a specific row of tiles
+export function reset_row_tiles(row_number) {
+  for (let p = 0; p < 5; p++) {
+    let letter_tile = rows_letter_tiles[row_number].children[p];
+    letter_tile.innerText = "";
+    letter_tile.style.backgroundColor = "lightgrey";
+  }
+}
+
+// Remove results
+export function clear_results() {
+  number_found_p.innerText = "";
+  words_found_p.innerText = "";
+  reveal_button.style.display = "None";
+}
+
+/*************************** Finding Results ***************************/
 export function find_word(list_elim_letters, correct_list, incorrect_dict) {
   let possible_words = [];
   for (var i in word_list) {
@@ -11,6 +111,7 @@ export function find_word(list_elim_letters, correct_list, incorrect_dict) {
     var letters_in_incorrect_place = check_incorrect_pos(word, incorrect_dict);
 
     if (
+      // if the word passes these 3 tests it is added to the list of possible words
       !has_eliminated_letters &&
       correct_letters_in_place &&
       !letters_in_incorrect_place
@@ -21,6 +122,7 @@ export function find_word(list_elim_letters, correct_list, incorrect_dict) {
   return possible_words;
 }
 
+// If a word has eliminated letters, returns true and the word is rejected
 function check_eliminated(word, list_elim_letters) {
   for (var letter in list_elim_letters) {
     if (word.includes(list_elim_letters[letter])) {
@@ -30,6 +132,7 @@ function check_eliminated(word, list_elim_letters) {
   return false;
 }
 
+// If a word has letters which are not in the correct place, returns false and the word is rejected
 function check_correct_pos(word, correct_list) {
   for (let i in correct_list) {
     let correct_letter = correct_list[i][0];
@@ -43,6 +146,7 @@ function check_correct_pos(word, correct_list) {
   return true;
 }
 
+// If a word has letters in inaccurate positions, returns true and the word is rejected
 function check_incorrect_pos(word, incorrect_list) {
   for (let i in incorrect_list) {
     let incorrect_letter = incorrect_list[i][0];
@@ -57,6 +161,45 @@ function check_incorrect_pos(word, incorrect_list) {
   return false;
 }
 
-export function remove_duplicates(list) {
-  return [...new Set(list)];
+// Check for duplicates before adding to eliminated, correct or incorrect letter lists
+export function add_to_list(list, element) {
+  let list_string = list.toString();
+  let element_string = element.toString();
+  if (list_string.indexOf(element_string) < 0) {
+    list.push(element);
+  }
+}
+
+// Return a list of tile colors to assist with finding results
+export function get_tile_colors(row) {
+  let p_colors = [];
+  for (let p = 1; p <= 9; p += 2) {
+    let bgColor = row.childNodes[3].childNodes[p].style.backgroundColor;
+    if (bgColor) {
+      p_colors.push(bgColor);
+    } else {
+      p_colors.push("lightgrey");
+    }
+  }
+  return p_colors;
+}
+
+// Return a list of rows with valid input
+export function get_valid_rows(number_guesses) {
+  let alpha_regex = /^[a-zA-Z]+$/; // regex expression to test that input is letters only
+  let valid_guess_rows = []; //
+  for (let i = 0; i < number_guesses; i++) {
+    let guess = all_guess_inputs[i].value;
+    if (guess.length == 5 && alpha_regex.test(guess)) {
+      valid_guess_rows.push(all_guess_rows[i]);
+    }
+  }
+  return valid_guess_rows;
+}
+
+/*************************** Displaying Results ***************************/
+// Show the number of possible words
+export function insert_results_summary(words_found) {
+  number_found_p.innerText = `${words_found.length} words were found. Would you like to reveal them ?`;
+  reveal_button.style.display = "block";
 }
--- wordgamehelper.js
+++ wordgamehelper.js
@@ -5,205 +5,152 @@ import {
   reset_button,
   num_of_guesses,
   guesses_section,
-  all_guess_rows,
-  all_guess_inputs,
-  number_found_p,
   words_found_p,
   reveal_button,
-  letter_squares,
 } from "./modules/const.js";
 
-import { find_word, remove_duplicates } from "./modules/funcs.js";
+import {
+  find_word,
+  reset_input,
+  reset_all_tiles,
+  clear_results,
+  add_to_list,
+  insert_results_summary,
+  get_valid_rows,
+  show_rows,
+  reset_rows,
+  change_color,
+  show_letters,
+  get_tile_colors,
+} from "./modules/funcs.js";
 
-var list_elim_letters = [];
-var incorrect_list = [];
-var correct_list = [];
-var list_previous_words = [];
+// ********* Variables necessary for storing the letters and results ********* //
+let list_elim_letters = [];
+let incorrect_list = [];
+let correct_list = [];
+let possible_words = [];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
-function clear_results() {
-  number_found_p.innerText = "";
-  words_found_p.innerText = "";
-  reveal_button.style.display = "None";
-}
-
-function reset_letter_squares() {
-  for (let i = 0; i < 25; i++) {
-    letter_squares[i].style.backgroundColor = "lightgrey";
-  }
-}
-
-// ****************** Change the number of guesses ****************** //
+// ****************** Listener for Changing Number of Guess Rows  ****************** //
 num_of_guesses.addEventListener("input", () => {
-  clear_results();
+  clear_results(); // in case a search has already been done, clear the results panel
+
   let number_guesses = parseInt(num_of_guesses.value);
-  let j = 1;
 
-  for (const child of guesses_section.children) {
-    console.log();
-    if (j <= number_guesses) {
-      child.style.display = "flex";
-    } else {
-      child.style.display = "none";
-
-      child.childNodes[2].value = "";
-      for (let p = 1; p <= 9; p += 2) {
-        child.childNodes[3].childNodes[p].innerText = "";
-        child.childNodes[3].childNodes[p].style.backgroundColor = "lightgrey";
-      }
-    }
-    j++;
-  }
-  if (number_found_p.innerText.length != 0) {
-    clear_results();
-  }
+  show_rows(number_guesses); // show the correct number of rows
+  reset_rows(number_guesses); // empty and hide unneeded guess rows
 });
 
-// ****************** Change the color of a letter ****************** //
-guesses_section.addEventListener("click", function (event) {
-  if (number_found_p.innerText.length != 0) {
-    clear_results();
-  }
-  // Get the target element that was clicked
-  var clickedElement = event.target;
-  if (clickedElement.localName == "p") {
-    let current_bgColor = clickedElement.style.backgroundColor;
-    if (current_bgColor == "yellow") {
-      clickedElement.style.backgroundColor = "lime";
-    } else if (current_bgColor == "lime") {
-      clickedElement.style.backgroundColor = "lightgrey";
-    } else if (current_bgColor == "" || current_bgColor == "lightgrey") {
-      clickedElement.style.backgroundColor = "yellow";
-    }
-  }
+// ***************** Listener for changing the color of a letter ***************** //
+guesses_section.addEventListener("click", (event) => {
+  clear_results(); // in case a search has already been done, clear the results panel
+
+  let clickedElement = event.target; // Get the target element that was clicked
+
+  change_color(clickedElement); // Change the background color of the tile
 });
 
-// ****************** Show Input in Letter Tiles ****************** //
-guesses_section.addEventListener("input", function (event) {
-  if (number_found_p.innerText.length != 0) {
-    clear_results();
-  }
-  // Get the target element that was clicked
-  var inputElement = event.target;
-  let row = inputElement.parentElement.parentElement;
-  let alpha_regex = /^[a-zA-Z]+$/;
-  if (!alpha_regex.test(inputElement.value)) {
-    for (const child of row.children) {
-      if (child.localName == "p") {
-        child.textContent = "";
-        child.style.backgroundColor = "lightgrey";
-        return null;
-      }
-    }
-  }
-  let j = 0;
-  for (const child of row.childNodes[3].children) {
-    if (child.localName == "p" && j < inputElement.value.length) {
-      child.textContent = inputElement.value[j].toUpperCase();
-      child.style.backgroundColor = "lightgrey";
-      j++;
-    } else if (child.localName == "p") {
-      child.textContent = "";
-      child.style.backgroundColor = "lightgrey";
-    }
-  }
+// ************************** Show Input in Letter Tiles ************************** //
+guesses_section.addEventListener("input", (event) => {
+  clear_results(); // in case a search has already been done, clear the results panel
+
+  let inputElement = event.target; // Get the target element that was changed
+  let row = inputElement.parentElement.parentElement; // Get the row of the element
+
+  show_letters(row, inputElement); // Show the letters in the tiles
 });
 
-// ****************** Find Possible Words ****************** //
+// ************************** Find Possible Words ************************** //
 find_button.addEventListener("click", () => {
-  let alpha_regex = /^[a-zA-Z]+$/;
+  // Clear any past results
+  list_elim_letters = [];
+  correct_list = [];
+  incorrect_list = [];
+
+  // verify the number of guesses selected to create a list of rows with valid input
   let number_guesses = parseInt(num_of_guesses.value);
-  let valid_guess_rows = [];
-  for (let i = 0; i < number_guesses; i++) {
-    let guess = all_guess_rows[i].childNodes[1].childNodes[2].value;
-    if (guess.length == 5 && alpha_regex.test(guess)) {
-      valid_guess_rows.push(all_guess_rows[i]);
-    }
-  }
-  let p_colors = [];
+  let valid_guess_rows = get_valid_rows(number_guesses);
+
   for (let i in valid_guess_rows) {
     let row = valid_guess_rows[i];
+    // This complex DOM reference is left in place rather than using the all inputs selector
+    // in case the user enters an invalid row in the middle of two or more valid rows
     let word = row.childNodes[1].childNodes[2].value.toLowerCase();
-    for (let p = 1; p <= 9; p += 2) {
-      let bgColor = row.childNodes[3].childNodes[p].style.backgroundColor;
-      if (bgColor) {
-        p_colors.push(bgColor);
-      } else {
-        p_colors.push("lightgrey");
-      } ///problem here is that p_colors doesn't reset with each row, fix this!
-    }
-    for (let i = 0; i < 5; i++) {
+
+    // Get a list of the colors of each letter tile
+    let p_colors = get_tile_colors(row);
+
+    // enumerate the color list to add the word letters to the correct list of letters
+    // which are either eliminated, correct, or included but in the incorrect place
+    for (let i in p_colors) {
       if (p_colors[i] == "yellow") {
         add_to_list(incorrect_list, [word[i], i]);
-        // incorrect_list.push();
       } else if (p_colors[i] == "lime") {
         add_to_list(correct_list, [word[i], i]);
-
-        // correct_list.push([word[i], i]);
       } else {
         add_to_list(list_elim_letters, word[i]);
-
-        // list_elim_letters.push(word[i]);
       }
     }
-    p_colors = [];
   }
+  // Make sure letters which are correct are not kept in the eliminated letters list which may happen if they repeat
+  let updated_list_elim_letters = Array.from(list_elim_letters);
+  for (let i in list_elim_letters) {
+    let elim_letter = list_elim_letters[i];
 
-  let possible_words = find_word(
-    list_elim_letters,
+    // correct and incorrect lists are nested lists, to search within the 2nd layer change them to a string
+    let correct_string = correct_list.toString();
+    let incorrect_string = incorrect_list.toString();
+
+    if (
+      //is true if the eliminated letter is in the string
+      correct_string.indexOf(elim_letter) >= 0 ||
+      incorrect_string.indexOf(elim_letter) >= 0
+    ) {
+      // remove the eliminated letter from the elimination list if it is a duplicate of a correct letter
+      let index_in_elimlist = updated_list_elim_letters.indexOf(elim_letter);
+      delete updated_list_elim_letters[index_in_elimlist];
+    }
+  }
+  //// need to rethink this to have it signal any repeated numbers to limit how many times they are used
+  /// look for double letters in the word, if any are found check to see if any are grey, if they are count how many, set the
+  /// limit based on this, add a final filter to each word where in runs through a list of lists and checks if X letter is used Y or more times
+  /// use string.match() to count
+
+  // Get possible word list
+  possible_words = find_word(
+    updated_list_elim_letters,
     correct_list,
     incorrect_list
   );
 
-  insert_results(possible_words);
-  list_elim_letters = [];
-  correct_list = [];
-  incorrect_list = [];
-  list_previous_words = Array.from(possible_words);
-  possible_words = [];
+  // Show the number of possible words and the button to reveal them
+  insert_results_summary(possible_words);
 });
 
-function insert_results(words_found) {
-  number_found_p.innerText = `${words_found.length} words were found. Would you like to reveal them ?`;
-  reveal_button.style.display = "block";
-}
-
+// ****************** Reveal the Possible Words   ****************** //
 reveal_button.addEventListener("click", () => {
-  let result = "";
-  for (let word in list_previous_words) {
-    result += list_previous_words[word] + ", ";
+  let result = ""; // to hold the complete result list as a string
+
+  // add each word tot he string
+  for (let word in possible_words) {
+    result += possible_words[word] + ", ";
   }
+
+  // insert the string into the results section HTML cutting off the final comma
   words_found_p.innerText = result.substring(0, result.length - 2);
 });
 
-function add_to_list(list, element) {
-  let list_string = list.toString();
-  let element_string = element.toString();
-  if (list_string.indexOf(element_string) < 0) {
-    list.push(element);
-  }
-}
-// ****************** Reset the Page - TO DO  ****************** //
+// ****************** Reset the Page   ****************** //
 reset_button.addEventListener("click", () => {
   reset_input();
-  reset_letter_squares();
+  reset_all_tiles();
   clear_results();
 });
 
+// ****************** Reset the Page on Page Refresh  ****************** //
 window.onload = reset_input();
-function reset_input() {
-  num_of_guesses.value = 1;
-  for (let i = 0; i < 5; i++) {
-    all_guess_inputs[i].value = "";
-  }
-  for (let i = 0; i < 25; i++) {
-    letter_squares[i].innerText = "";
-  }
 
-  for (let i = 1; i <= 4; i++) {
-    all_guess_rows[i].style.display = "None";
-  }
-}
+// ****************** Prevent Default Behavior  ****************** //
+// There is no submit button, but keep this to avoid any unexpected behavior
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
